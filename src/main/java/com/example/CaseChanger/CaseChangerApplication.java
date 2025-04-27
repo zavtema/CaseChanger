8167c6271edf4c03819284a4b5f28e09a2d71149
zavtema <zaviruxaartemii@gmail.com>
2025-04-27 16:58:02 +0300

    Начальная стадия проекта/создание

---
+++ src/main/java/com/example/CaseChanger/CaseChangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaseChanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseChangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CaseChanger/CaseChangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CaseChanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseChangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
